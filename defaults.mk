76e369f34237db57b262f8525a6d47b74c13ca4d
Yossi Kreinin <Yossi.Kreinin@gmail.com>
2013-03-30 07:48:28 -0300

    Update defaults.mk

--- defaults.mk
+++ defaults.mk
@@ -1,3 +1,3 @@
 export CT_VALGRIND_SRC_DIR?=# write your valgrind source distribution root here
 export VALGRIND_LIB?=/usr/lib/valgrind#you can change it to your library directory
-export CT_VALGRIND_CP?=sudo cp#or plain cp if $(CT_VALGRIND_LIB_DIR) is writeable for you
+export CT_VALGRIND_CP?=sudo cp#or plain cp if $(VALGRIND_LIB) is writeable for you
